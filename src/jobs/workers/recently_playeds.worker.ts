64d1047995bb2ec5c6347f899e7174e8780c439f
Joilson Leal <joilson_rg@hotmail.com>
2021-09-03 11:59:42 -0300

    ref: worker limit

--- src/jobs/workers/recently_playeds.worker.ts
+++ src/jobs/workers/recently_playeds.worker.ts
@@ -28,7 +28,7 @@ async function runWorker() {
   console.log('Starting worker');
   connection = await getConnection();
   let iteration = 0;
-  const limit = 80;
+  const limit = 50;
   while (true) {
     try {
       const usersData = await connection.query(
