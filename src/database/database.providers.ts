a42e35011b202d6e34a3c9430fb03e3f3cde98b0
Joilson Leal <joilson_rg@hotmail.com>
2021-09-21 21:42:04 -0300

    fix: db

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () =>
       await createConnection({
         type: 'mysql',
-        host: 'f429klen0n10vvrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', //process.env.DB_HOST,
+        host: process.env.DB_HOST,
         port: 3306,
-        username: 'tb60powkvrg1ptfn', //process.env.DB_USER,
-        password: 'knv9cbqux70omrm3', //process.env.DB_PASS,
-        database: 'w9a8xpy6biprbkvf', //process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: ['error'],
