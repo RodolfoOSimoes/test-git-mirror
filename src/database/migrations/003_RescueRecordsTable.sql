7f478a5ecb712de84b59e1d4661097fd9f4a365d
Joilson Leal <joilson_rg@hotmail.com>
2021-09-24 09:39:13 -0300

    fix: rename and fix rescue records script

--- src/database/migrations/003_RescueRecordsTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS rescue_records (
-  id         BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
-  quantity   INT NOT NULL,
-  date       DATE NOT NULL,
-  created_at DATETIME NOT NULL,
-  updated_at DATETIME NOT NULL
-)
