8ab032d3f769448c06fcda5bb9c2027e0e7958c2
Joilson Leal <joilson_rg@hotmail.com>
2021-09-26 12:05:34 -0300

    fix: transaction

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,7 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors();
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  });
 
-  //  apply to all requests
-  app.use(limiter);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
