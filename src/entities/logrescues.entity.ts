4f5dccbc646873348c9eeadb9036adcc33963bdc
= <=>
2021-12-07 11:23:00 -0300

    Debugando

--- src/entities/logrescues.entity.ts
+++ src/entities/logrescues.entity.ts
@@ -2,6 +2,8 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn
 } from 'typeorm';
 
 @Entity('admins')
@@ -11,4 +13,10 @@ export class Admin {
 
   @Column({ nullable: true })
   user_id: number;
+
+  @CreateDateColumn({ type: 'datetime', nullable: true })
+  created_at: Date;
+
+  @Column({ nullable: true })
+  qtd_product_purchased: number;
 }
