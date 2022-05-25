5dcfe0a5a687772ba2645fd1b8484e1a0fd58c51
Rodolfo Simões <rodolfo.simoes@druid.com.br>
2022-05-25 16:35:31 -0300

    Fixed service when user address not found

--- src/client/address/address.service.ts
+++ src/client/address/address.service.ts
@@ -34,37 +34,39 @@ export class AddressService {
 
   async findOne(id: number) {
     const user = await this.userRepository.findOne(id);
-
     const address = await this.addressRepository.findOne({
       where: { user: user },
       order: { id: 'DESC' },
       relations: ['city', 'city.state'],
     });
 
-    const data = {
-      id: address.id,
-      cep: address.cep,
-      city: {
-        id: address.city?.id,
-        name: address.city?.name,
-        state: {
-          id: address.city?.state?.id,
-          acronym: address.city?.state?.acronym,
+    let addressData: any = null;
+    if (address) {
+      addressData = {
+        id: address.id,
+        cep: address.cep,
+        city: {
+          id: address.city?.id,
+          name: address.city?.name,
+          state: {
+            id: address.city?.state?.id,
+            acronym: address.city?.state?.acronym,
+          },
         },
-      },
-      complement: address.complement,
-      cpf: address.cpf,
-      created_at: address.created_at,
-      full_address: address.full_address,
-      neighborhood: address.neighborhood,
-      number: address.number,
-      recipient: address.recipient,
-      reference: address.reference,
-      street: address.street,
-      updated_at: address.updated_at,
-    };
+        complement: address.complement,
+        cpf: address.cpf,
+        created_at: address.created_at,
+        full_address: address.full_address,
+        neighborhood: address.neighborhood,
+        number: address.number,
+        recipient: address.recipient,
+        reference: address.reference,
+        street: address.street,
+        updated_at: address.updated_at,
+      };
+    }
 
-    return data;
+    return addressData;
   }
 
   async update(id: number, dto: UpdateAddressDto) {
